0fec35ff90fef05d5c82bb633b0d4903e1e7ed40
PatipanMaomode <patipanmaomod@gmail.com>
2026-03-26 16:26:06 +0700

    add augment

--- dataset.py
+++ dataset.py
@@ -1,9 +1,11 @@
 import torch
 from torch.utils.data import Dataset
 import torchaudio
+import torchaudio.transforms as T
 import pandas as pd
 import os
 from config import F5Config
+import random
 
 
 class VoiceDataset(Dataset):
@@ -17,6 +19,7 @@ class VoiceDataset(Dataset):
             chars = [l.strip() for l in f if l.strip()]
         self.char2id = {c: i+1 for i, c in enumerate(chars)}  # 0 = pad
         self.vocab_size = len(self.char2id)
+
 
         # โหลด metadata
         df = pd.read_csv(f'{self.data_path}/metadata.csv', sep="|",
@@ -49,7 +52,9 @@ class VoiceDataset(Dataset):
         wav, sr = torchaudio.load(audio_path)
         if sr != self.sample_rate:
             wav = torchaudio.functional.resample(wav, sr, self.sample_rate)
-        wav = wav.mean(0)  # mono
+
+        #augment
+        wav = augment_audio(wav, self.sample_rate)
 
         mel = self.mel(wav)                    # [n_mel, T]
         mel = torch.log(mel.clamp(min=1e-5))
@@ -86,3 +91,29 @@ def collate_fn(batch):
         tok_pad[i, :t.size(0)] = t
 
     return mel_pad, tok_pad, mask
+
+
+
+
+
+
+def augment_audio(wav, sample_rate=24000, pitch_range=2, speed_range=0.05):
+    """
+    wav: Tensor [1, T] หรือ [T]
+    pitch_range: สูงสุด ±จำนวน semitone
+    speed_range: ±% ของความเร็ว
+    """
+    # 1. Random pitch shift
+    semitone_shift = random.uniform(-pitch_range, pitch_range)
+    if semitone_shift != 0:
+        wav = T.Resample(orig_freq=sample_rate, new_freq=int(sample_rate * 2 ** (semitone_shift/12)))(wav)
+
+    # 2. Random speed change
+    speed_factor = random.uniform(1-speed_range, 1+speed_range)
+    if speed_factor != 1:
+        wav = T.Resample(orig_freq=sample_rate, new_freq=int(sample_rate*speed_factor))(wav)
+
+    # 3. ปรับให้กลับมาที่ sample_rate เดิม
+    wav = T.Resample(orig_freq=wav.shape[-1], new_freq=sample_rate)(wav) if wav.shape[-1] != sample_rate else wav
+
+    return wav
